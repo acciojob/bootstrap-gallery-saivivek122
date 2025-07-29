27bc5c75476d021b3e6729859fd40c3a69d6f83d
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-07-29 07:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+let images=document.querySelectorAll(".card-img-top");
+
+let carousel=document.querySelector(".carousel.slide")
+
+let cardImages=document.querySelector(".card-group");
+
+let carouselItem=document.querySelectorAll(".carousel-item");
+
+
+
+images.forEach(img=>{
+  img.addEventListener("click",()=>{
+    let id=parseInt(img.id);
+    // console.log(id)
+    carousel.classList.remove("d-none")
+    cardImages.classList.add("d-none");
+
+    carouselItem.forEach(item=>item.classList.remove("active"))
+
+    if(carouselItem[id]){
+      carouselItem[id].classList.add("active")
+    }
+    
+
+    
+  })
+})
+
+document.addEventListener("click", function (event) {
+  if (!carousel.contains(event.target) &&!event.target.classList.contains("card-img-top")) {
+    carousel.classList.add("d-none");
+    cardImages.classList.remove("d-none");
+  }
+});
